6bb384377acd76309f9dee11e7b930c9666491c6
huyhoang2401x <huyhoang.2401x@gmail.com>
2023-02-22 23:55:49 +0700

    CRUD theloai

--- admin/add_category.php
+++ admin/add_category.php
@@ -44,13 +44,14 @@
     </header>
     <main class="container mt-5 mb-5">
         <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
+      
         <div class="row">
             <div class="col-sm">
                 <h3 class="text-center text-uppercase fw-bold">Thêm mới thể loại</h3>
                 <form action="process_add_category.php" method="post">
                     <div class="input-group mt-3 mb-3">
                         <span class="input-group-text" id="lblCatName">Tên thể loại</span>
-                        <input type="text" class="form-control" name="txtCatName" >
+                        <input type="text" class="form-control" name="tentheloai" >
                     </div>
 
                     <div class="form-group  float-end ">
@@ -58,6 +59,7 @@
                         <a href="category.php" class="btn btn-warning ">Quay lại</a>
                     </div>
                 </form>
+                
             </div>
         </div>
     </main>
