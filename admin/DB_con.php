d12e09df71813daed35d9055d41dbd2ae62322cc
hoang hiep <1951060695@e.tlu.edu.vn>
2023-02-26 23:58:15 +0700

    update DB

--- admin/DB_con.php
+++ admin/DB_con.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "btth01_cse485_ex";
+    $dbname = "btth01_cse485";
     $conn = mysqli_connect($servername, $username, $password);
     mysqli_select_db($conn, $dbname);
     mysqli_query($conn, "SET NAMES 'utf8'");
