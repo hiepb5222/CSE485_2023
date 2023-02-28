caa062b09f7d37f714f6024c2d1e273d8e733f0d
unknown <duchai2211@gmail.com>
2023-02-28 13:16:46 +0700

    updatesendmail

--- sendmail.php
+++ sendmail.php
@@ -1,9 +1,11 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
 function send($mailR)
 {
   $mail = new PHPMailer(true);
@@ -14,12 +16,12 @@ function send($mailR)
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'duchai2211@gmail.com'; // Replace with your Gmail email address
-    $mail->Password = 'mpyoumbwdvwmjiua'; // Replace with your Gmail password or app-specific password
+    $mail->Password = 'rnqjdstpzvychuam';  // Replace with your Gmail password or app-specific password
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
     // Email content
-    $mail->setFrom('dinhduy2012001@gmail.com', 'Your Name');
+    $mail->setFrom('duchai2211@gmail.com', 'Your Name');
     $mail->addAddress($mailR, 'Recipient Name');
     $mail->Subject = 'Test Email';
     $mail->Body = 'This is a test email message';
